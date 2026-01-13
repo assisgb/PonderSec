281f7bad5ea1221d8ce60c238bae194f9bf615b2
Luizzbar <luiz.costa@icomp.ufam.edu.br>
2026-01-12 22:27:26 -0400

    fazendo umas loucurinhas

--- responsegenerator/urls.py
+++ responsegenerator/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('pondersecoptions/', views.pondersecoptions, name="pondersecoptions"),
     path('perguntar/', views.perguntar, name="perguntar"),
     path('historico/', views.historico, name="historico"),
-
+    path('historico/deletar/<int:id>/', views.deletar_item_historico, name='deletar_item_historico'),
+    path('historico/ver/<int:id>/', views.ver_detalhes, name='ver_detalhes'),
+    path('historico/limpar/', views.limpar_historico, name='limpar_historico'),
 ]
