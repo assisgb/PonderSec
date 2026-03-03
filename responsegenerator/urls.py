53361dba29b4716aa870012b5a28421e49acd780
assisgb <gabrielassisdev@gmail.com>
2026-03-03 12:10:01 -0400

    finalização setup avaliação

--- responsegenerator/urls.py
+++ responsegenerator/urls.py
@@ -11,11 +11,15 @@ urlpatterns = [
     path('executar_consulta', views.executar_consulta, name="executar_consulta"),
     path('avaliacao/', views.avaliacao, name="avaliacao"),
     path('avaliacao_respostas/', views.avaliacao_respostas, name="avaliacao_respostas"),
+    path('setup-adicionar-metrica/', views.setup_adicionar_metrica, name='setup_adicionar_metrica'),
+    path('setup-configurar-metrica/', views.setup_configurar_metrica, name='setup_configurar_metrica'),
     path('setup/', views.setup, name="setup"),
     path('setup_llm/', views.setup_llm, name="setup_llm"),
+    path('setup_configurar_llm/', views.setup_configurar_llm, name="setup_configurar_llm"),
     path('setup_avaliacao/', views.setup_avaliacao, name="setup_avaliacao"),
     path('historico/', views.historico, name="historico"),
     path('historico/deletar/<int:id>/', views.deletar_item_historico, name='deletar_item_historico'),
     path('historico/ver/<int:id>/', views.ver_detalhes, name='ver_detalhes'),
     path('historico/limpar/', views.limpar_historico, name='limpar_historico'),
+    
 ]
